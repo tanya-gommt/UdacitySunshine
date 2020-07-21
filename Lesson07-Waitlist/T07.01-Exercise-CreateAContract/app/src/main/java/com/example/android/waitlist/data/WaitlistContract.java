ea64b2c29569d6506b9b540796ac19106258ac2f
mmt8627 <Tanya.Sharma@go-mmt.com>
2020-07-21 18:58:35 +0530

    Content Providers

--- Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -4,7 +4,7 @@ import android.provider.BaseColumns;
 
 public class WaitlistContract {
 
-
+//
 
     // TODO (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
 
