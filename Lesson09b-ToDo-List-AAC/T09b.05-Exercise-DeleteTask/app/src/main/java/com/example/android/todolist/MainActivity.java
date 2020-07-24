be9eba340e1e9bbadcb9aa67183c819789c1ef7c
mmt8627 <Tanya.Sharma@go-mmt.com>
2020-07-24 10:34:51 +0530

    T09b.05 Solution

--- Lesson09b-ToDo-List-AAC/T09b.05-Exercise-DeleteTask/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.05-Exercise-DeleteTask/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -76,10 +76,19 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
 
             // Called when a user swipes left or right on a ViewHolder
             @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
                 // Here is where you'll implement swipe to delete
                 // TODO (1) Get the diskIO Executor from the instance of AppExecutors and
                 // call the diskIO execute method with a new Runnable and implement its run method
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        int position = viewHolder.getAdapterPosition();
+                        List<TaskEntry> tasks = mAdapter.getTasks();
+                        mDb.taskDao().deleteTask(tasks.get(position));
+                        retrieveTasks();
+                    }
+                });
 
                 // TODO (3) get the position from the viewHolder parameter
                 // TODO (4) Call deleteTask in the taskDao with the task at that position
@@ -115,6 +124,10 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
     protected void onResume() {
         super.onResume();
         // TODO (5) Extract the logic to a retrieveTasks method so it can be reused
+        retrieveTasks();
+    }
+
+    private void retrieveTasks() {
         AppExecutors.getInstance().diskIO().execute(new Runnable() {
             @Override
             public void run() {
